f9493cee8089f995e617242024b533fd49a2bd75
sweir17 <scottweir05@gmail.com>
2015-02-16 14:42:47 -0800

    1.8 Exercise complete

--- PhoneDirectory/PD_Application.cpp
+++ PhoneDirectory/PD_Application.cpp
@@ -91,7 +91,7 @@ void do_remove_entry(Phone_Directory& the_directory) // Exercise 1.8: please com
 	cout << "Enter name: ";
 	getline(cin, name);
 	// Complete the rest of this function 
-	
+	string x = the_directory.remove_entry(name);
 }
 
 void do_save(Phone_Directory& the_directory)
